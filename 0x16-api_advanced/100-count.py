5fedc9219a135cac052c89d33c5ee56e6a33b1b9
Wambui Gachora <wggachora@gmail.com>
2023-08-10 05:30:20 -0700

    correcting advanced task

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,34 +1,36 @@
 #!/usr/bin/python3
-"""
-Function to count words in all hot posts of given subreddit."""
+"""Function to count words in all hot posts of a given Reddit subreddit."""
 import requests
 
 
 def count_words(subreddit, word_list, instances={}, after="", count=0):
-    """Prints count of given words found in hot posts of given subreddit.
-
+    """Prints counts of given words found in hot posts of a given subreddit.
     Args:
-        subreddit (strt): the subreddit to search.
-        word_list (list): The list of words to search for in post titles
-        found_list (obj): Key/value pair for words/count.
-        after (str): The parameter for next page of API results
+        subreddit (str): The subreddit to search.
+        word_list (list): The list of words to search for in post titles.
+        instances (obj): Key/value pairs of words/counts.
+        after (str): The parameter for the next page of the API results.
+        count (int): The parameter of results matched thus far.
     """
-    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
-    headers = {'User-Agent': 'linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)'}
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
     params = {
-            'after': after,
-            'count': count,
-            'limit': 100
-            }
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
     try:
         results = response.json()
         if response.status_code == 404:
             raise Exception
     except Exception:
-            print("")
-            return
-    
+        print("")
+        return
+
     results = results.get("data")
     after = results.get("after")
     count += results.get("dist")
@@ -40,13 +42,13 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
                 if instances.get(word) is None:
                     instances[word] = times
                 else:
-                    instance[word] += times
-    
+                    instances[word] += times
+
     if after is None:
         if len(instances) == 0:
             print("")
             return
-        instances = sorted(instances.items(), key=lambda kv:(-kv[1], kv[0]))
+        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
         [print("{}: {}".format(k, v)) for k, v in instances]
     else:
         count_words(subreddit, word_list, instances, after, count)
